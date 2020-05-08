416d8a43b4371fa6ec7ec41d45edbfed9c37db1c
kk-mbugua <kamau.dev@gmail.com>
2020-05-08 09:30:56 -0700

    server reorganization

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const path = require("path");
 const bodyParser = require("body-parser");
-const Order = require("./database/models/order_model")
 
 
 const app = express()
